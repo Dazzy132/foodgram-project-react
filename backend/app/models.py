339b680a8207285494e63d28fd003a47c93776aa
Влад Соловьёв <vladsolo23rus@gmail.com>
2023-03-13 21:59:19 +0300

    Изменение сортировки

--- backend/app/models.py
+++ backend/app/models.py
@@ -30,7 +30,7 @@ class Ingredient(models.Model):
     class Meta:
         verbose_name = 'Ингридиент'
         verbose_name_plural = 'Ингридиенты'
-        ordering = ('pk',)
+        ordering = ('id',)
 
     def __str__(self):
         return f'{self.name} | {self.measurement_unit}'
